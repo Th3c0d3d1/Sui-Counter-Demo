3e3755bce109edf828b2b611fda93238aec563b4
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-11-20 16:01:15 -0500

    add constants file for front end, adj network config for constants

--- my-first-sui-dapp/src/networkConfig.ts
+++ my-first-sui-dapp/src/networkConfig.ts
@@ -1,16 +1,24 @@
 import { getFullnodeUrl } from "@mysten/sui/client";
+import { 
+  DEVNET_COUNTER_PACKAGE_ID,
+  TESTNET_COUNTER_PACKAGE_ID,
+  MAINNET_COUNTER_PACKAGE_ID
+} from "./constants";
 import { createNetworkConfig } from "@mysten/dapp-kit";
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
     devnet: {
       url: getFullnodeUrl("devnet"),
+      variables: {DEVNET_COUNTER_PACKAGE_ID},
     },
     testnet: {
       url: getFullnodeUrl("testnet"),
+      variables: {TESTNET_COUNTER_PACKAGE_ID},
     },
     mainnet: {
       url: getFullnodeUrl("mainnet"),
+      variables: {MAINNET_COUNTER_PACKAGE_ID},
     },
   });
 
